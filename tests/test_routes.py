3018f9f48e2086bec30813414d0e03043993c467
Krishnapriya Bandewar <krishnapriya.22211528@viit.ac.in>
2024-10-18 23:24:57 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,5 @@
 ######################################################################
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,165 +14,45 @@
 # limitations under the License.
 ######################################################################
 
-# spell: ignore Rofrano jsonify restx dbname
 """
-Product Store Service with UI
+Product Steps
+
+Steps file for products.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
 """
-from flask import jsonify, request, abort
-from flask import url_for  # noqa: F401 pylint: disable=unused-import
-from service.models import Product
-from service.common import status  # HTTP Status Codes
-from . import app
+import requests
+from behave import given
 
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
 
-######################################################################
-# H E A L T H   C H E C K
-######################################################################
-@app.route("/health")
-def healthcheck():
-    """Let them know our heart is still beating"""
-    return jsonify(status=200, message="OK"), status.HTTP_200_OK
-
-
-######################################################################
-# H O M E   P A G E
-######################################################################
-@app.route("/")
-def index():
-    """Base URL for our service"""
-    return app.send_static_file("index.html")
-
-
-######################################################################
-#  U T I L I T Y   F U N C T I O N S
-######################################################################
-def check_content_type(content_type):
-    """Checks that the media type is correct"""
-    if "Content-Type" not in request.headers:
-        app.logger.error("No Content-Type specified.")
-        abort(
-            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            f"Content-Type must be {content_type}",
-        )
-
-    if request.headers["Content-Type"] == content_type:
-        return
-
-    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {content_type}",
-    )
-
-
-######################################################################
-# C R E A T E   A   N E W   P R O D U C T
-######################################################################
-@app.route("/products", methods=["POST"])
-def create_products():
-    """
-    Creates a Product
-    This endpoint will create a Product based the data in the body that is posted
-    """
-    app.logger.info("Request to Create a Product...")
-    check_content_type("application/json")
-
-    data = request.get_json()
-    app.logger.info("Processing: %s", data)
-    product = Product()
-    product.deserialize(data)
-    product.create()
-    app.logger.info("Product with new id [%s] saved!", product.id)
-
-    message = product.serialize()
+@given('the following products')
+def step_impl(context):
+    """ Delete all Products and load new ones """
+    #
+    # List all of the products and delete them one by one
+    #
+    rest_endpoint = f"{context.base_url}/products"
+    context.resp = requests.get(rest_endpoint)
+    assert(context.resp.status_code == HTTP_200_OK)
+    for product in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
+        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
 
     #
-    # Uncomment this line of code once you implement READ A PRODUCT
+    # load the database with new products
     #
-    # location_url = url_for("get_products", product_id=product.id, _external=True)
-    location_url = "/"  # delete once READ is implemented
-    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
-
-
-######################################################################
-# L I S T   A L L   P R O D U C T S
-######################################################################
-
-#
-# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
-#
-
-######################################################################
-# R E A D   A   P R O D U C T
-######################################################################
-
-app.route("/products/<int:product_id>", methods=["GET"])
-
-def get_products(product_id):
-
-Retrieve a single Product
-
-This endpoint ll return a Product
-
-app.logger.info("Request to Retrieve product with id [%s]", product_id)
-
-product Product.find(product id)
-if not product:
-  abort(status .HTTP_404_NOT_FOUND "Product with id product id) was not found.")
-
-app.logger.info("Returning product: %s", product.name)
-
-return product.serialize(), status.HTTP_200_OK
-
-######################################################################
-# U P D A T E   A   P R O D U C T
-######################################################################
-
-@app.route("/products/<int:product_id", methods=["PUT"])
-
-def update products (product_id):
-
-Update a Product
-
-This endpoint will update a Product based the body that is posted
-
-app.logger.info("Request to Update a product with j [%S],product_id)
-
-check_content_type("application/json")
-                
-product Product.find(product_id)
-
-If not product:
-
-abort(status. HTTP_404_NOT_FOUND,f"Product with id (product id)" was not found.")
-
-data=request.get_json()
-
-app.logger.info(data)
-
-product.deserialize(data)
-
-product.id product_id
-
-product.update()
-
-return make response(Jsonify(product.serialize()), status.HTTP_200_OK)
-######################################################################
-# D E L E T E   A   P R O D U C T
-######################################################################
-@app.route("/products/<int:product_id>", methods=["DELETE"])
-def delete_products (product_id):
-
-Delete a Product
-
-This endpoint will delete a Product dased the id specified in the path
-
-app.logger.info("Request to delete a product with id [%s]", product_id)
-
-product Product.find(product_id)
-
-if product:
-   product.delete() 
-
-
-return make_response("", status.HTTP_284_NO_CONTENT)
+    for row in context.table:
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
